d14a56836b3a9a6e8f27dfc5d40a6eddea7e8b13
assadribhi <assadribhi@gmail.com>
2020-09-23 17:50:09 +0300

    Removed quotation marks

--- db/db.js
+++ db/db.js
@@ -7,7 +7,7 @@ const db = process.env.DATABASE_URL
     })
   : new Sequelize({
       username: "postgres",
-      password: "",
+      password: "@$$@D@$$@d",
       database: "goals_db",
       dialect: "postgres",
       host: "localhost",
