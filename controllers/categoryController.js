1491e708b1d35684f3b282d3bd656172232a6435
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2020-09-26 12:06:05 -0700

    added a category list

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,16 @@
 const { Category } = require("../db/models");
 
+exports.categoryList = async (req, res, next) => {
+  try {
+    const categoriesList = await Category.findAll({
+      attributes: {
+        exclude: ["updatedAt", "createdAt"],
+      },
+    });
+    res.json(categoriesList);
+  } catch (error) {}
+};
+
 exports.createCategory = async (req, res, next) => {
   try {
     const newCategory = await Category.create(req.body);
