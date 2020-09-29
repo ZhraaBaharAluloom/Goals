dca7043014c98ce0f6e86420062b333a10feeaf5
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2020-09-29 09:02:48 -0700

    added a tag controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,10 +1,24 @@
 const { Category } = require("../db/models");
+const Goal = require("../db/models/Goal");
+
+exports.fetchCategory = async (categoryId, next) => {
+  try {
+    const category = await Category.findByPk(categoryId);
+    return category;
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.categoryList = async (req, res, next) => {
   try {
     const categoriesList = await Category.findAll({
       attributes: {
         exclude: ["updatedAt", "createdAt"],
+      },
+      include: {
+        model: Goal,
+        attributes: { exclude: ["createdAt", "updatedAt"] },
       },
     });
     res.json(categoriesList);
